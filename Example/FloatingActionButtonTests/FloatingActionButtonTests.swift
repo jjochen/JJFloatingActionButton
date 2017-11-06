6903c31904d07904ab1186d5db59e41085a0ca9a
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2017-11-06 11:57:21 +0100

    simple test, travis configuration

--- Example/FloatingActionButtonTests/FloatingActionButtonTests.swift
+++ Example/FloatingActionButtonTests/FloatingActionButtonTests.swift
@@ -6,30 +6,28 @@
 //  Copyright © 2017 Jochen Pfeiffer. All rights reserved.
 //
 
-import XCTest
-@testable import FloatingActionButton
+import Quick
+import Nimble
+import Nimble_Snapshots
 
-class FloatingActionButtonTests: XCTestCase {
+@testable import JJFloatingActionButton
+
+class FloatingActionButtonTests: QuickSpec {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    override func spec() {
+        
+        describe("FloatingActionButton") {
+            
+            var view: JJFloatingActionButton!
+            
+            beforeEach {
+                view = JJFloatingActionButton()
+                view.translatesAutoresizingMaskIntoConstraints = false
+            }
+            
+            it("should be there") {
+                expect(view) == snapshot()
+            }
         }
     }
     
