d6dc68309571a918e6f71e63adc63539c1c2fa7f
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2017-12-06 17:46:54 +0100

    swiftformat

--- Example/JJFloatingActionButton/SecondViewController.swift
+++ Example/JJFloatingActionButton/SecondViewController.swift
@@ -6,17 +6,17 @@
 //  Copyright © 2017 Jochen Pfeiffer. All rights reserved.
 //
 
-import UIKit
 import JJFloatingActionButton
+import UIKit
 
 class SecondViewController: UIViewController {
-    
-    @IBOutlet weak var actionButton: JJFloatingActionButton!
-    
+
+    @IBOutlet var actionButton: JJFloatingActionButton!
+
     override func viewDidLoad() {
-        
+
         actionButton.addItem(title: "item", image: UIImage(named: "Second")?.withRenderingMode(.alwaysTemplate)) { item in
-            
+
             let alertController = UIAlertController(title: item.titleLabel.text, message: "button tapped!", preferredStyle: .alert)
             alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
             self.present(alertController, animated: true, completion: nil)
