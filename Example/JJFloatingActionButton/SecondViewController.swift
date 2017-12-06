5e89133a6b54c755657f4be7bb997cfafaa80c8e
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2017-12-06 16:35:53 +0100

    add action button to second view controller via interface builder, make images used in example render as template, remove example tests

--- Example/JJFloatingActionButton/SecondViewController.swift
+++ Example/JJFloatingActionButton/SecondViewController.swift
@@ -7,6 +7,19 @@
 //
 
 import UIKit
+import JJFloatingActionButton
 
 class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var actionButton: JJFloatingActionButton!
+    
+    override func viewDidLoad() {
+        
+        actionButton.addItem(title: "item", image: UIImage(named: "Second")?.withRenderingMode(.alwaysTemplate)) { item in
+            
+            let alertController = UIAlertController(title: item.titleLabel.text, message: "button tapped!", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
 }
