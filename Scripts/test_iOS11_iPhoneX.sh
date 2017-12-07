712f4c114e349b44ef5dca2169bb691fcfba4139
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2017-12-07 23:01:43 +0100

    fix circle ci config

--- Scripts/test_iOS11_iPhoneX.sh
+++ Scripts/test_iOS11_iPhoneX.sh
@@ -8,7 +8,7 @@ xcodebuild clean build test  \
   -workspace "Example/JJFloatingActionButton.xcworkspace" \
   -scheme "JJFloatingActionButton_Example" \
   -sdk iphonesimulator \
-  -destination "OS=11.0.1,name=iPhone X" \
+  -destination "OS=11.0,name=iPhone X" \
   -configuration Debug \
   -enableCodeCoverage YES \
   ONLY_ACTIVE_ARCH=YES \
