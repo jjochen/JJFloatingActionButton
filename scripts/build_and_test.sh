4eace76a43d4fc9b13fefa4ef4d08cace9660385
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2017-12-08 10:02:04 +0100

    test circle ci

---
+++ scripts/build_and_test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+xcodebuild -version
+
+xcodebuild -showsdks
+
+xcodebuild clean build test \
+  -workspace Example/JJFloatingActionButton.xcworkspace \
+  -scheme JJFloatingActionButton_Example \
+  -sdk iphonesimulator \
+  -destination "$DESTINATION" \
+  -enableCodeCoverage YES \
+  CODE_SIGNING_REQUIRED=NO \
+  CODE_SIGN_IDENTITY= \
+  PROVISIONING_PROFILE=
