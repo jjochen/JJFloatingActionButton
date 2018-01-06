0cb21276a5501ecdc59618cf7022ca57ed65947c
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2018-01-06 23:00:06 +0100

    less tolerance for comparing snapshots

--- Example/Tests/JJFloatingActionButtonSpec.swift
+++ Example/Tests/JJFloatingActionButtonSpec.swift
@@ -46,7 +46,7 @@ class JJFloatingActionButtonSpec: QuickSpec {
                 actionButton = JJFloatingActionButton(frame: actionButtonFrame)
                 superview.addSubview(actionButton)
 
-                setNimbleTolerance(0.004)
+                setNimbleTolerance(0.001)
             }
 
             it("does not open") {
