712b461c10e9747955a8d6f2cfc87a59443ebdb3
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2019-09-09 13:28:18 +0200

    fix dynamic constraints creation of action item

--- Sources/JJActionItem.swift
+++ Sources/JJActionItem.swift
@@ -306,38 +306,38 @@ fileprivate extension JJActionItem {
 
         switch titlePosition {
         case .leading:
-            dynamicConstraints.append(circleView.trailingAnchor.constraint(equalTo: trailingAnchor))
-            dynamicConstraints.append(circleView.centerYAnchor.constraint(equalTo: centerYAnchor))
-            dynamicConstraints.append(titleLabel.trailingAnchor.constraint(equalTo: circleView.leadingAnchor, constant: -horizontalSpacing))
-            dynamicConstraints.append(titleLabel.centerYAnchor.constraint(equalTo: circleView.centerYAnchor))
+            constraints.append(circleView.trailingAnchor.constraint(equalTo: trailingAnchor))
+            constraints.append(circleView.centerYAnchor.constraint(equalTo: centerYAnchor))
+            constraints.append(titleLabel.trailingAnchor.constraint(equalTo: circleView.leadingAnchor, constant: -horizontalSpacing))
+            constraints.append(titleLabel.centerYAnchor.constraint(equalTo: circleView.centerYAnchor))
         case .trailing:
-            dynamicConstraints.append(circleView.leadingAnchor.constraint(equalTo: leadingAnchor))
-            dynamicConstraints.append(circleView.centerYAnchor.constraint(equalTo: centerYAnchor))
-            dynamicConstraints.append(titleLabel.leadingAnchor.constraint(equalTo: circleView.trailingAnchor, constant: horizontalSpacing))
-            dynamicConstraints.append(titleLabel.centerYAnchor.constraint(equalTo: circleView.centerYAnchor))
+            constraints.append(circleView.leadingAnchor.constraint(equalTo: leadingAnchor))
+            constraints.append(circleView.centerYAnchor.constraint(equalTo: centerYAnchor))
+            constraints.append(titleLabel.leadingAnchor.constraint(equalTo: circleView.trailingAnchor, constant: horizontalSpacing))
+            constraints.append(titleLabel.centerYAnchor.constraint(equalTo: circleView.centerYAnchor))
         case .left:
-            dynamicConstraints.append(circleView.rightAnchor.constraint(equalTo: rightAnchor))
-            dynamicConstraints.append(circleView.centerYAnchor.constraint(equalTo: centerYAnchor))
-            dynamicConstraints.append(titleLabel.rightAnchor.constraint(equalTo: circleView.leftAnchor, constant: -horizontalSpacing))
-            dynamicConstraints.append(titleLabel.centerYAnchor.constraint(equalTo: circleView.centerYAnchor))
+            constraints.append(circleView.rightAnchor.constraint(equalTo: rightAnchor))
+            constraints.append(circleView.centerYAnchor.constraint(equalTo: centerYAnchor))
+            constraints.append(titleLabel.rightAnchor.constraint(equalTo: circleView.leftAnchor, constant: -horizontalSpacing))
+            constraints.append(titleLabel.centerYAnchor.constraint(equalTo: circleView.centerYAnchor))
         case .right:
-            dynamicConstraints.append(circleView.leftAnchor.constraint(equalTo: leftAnchor))
-            dynamicConstraints.append(circleView.centerYAnchor.constraint(equalTo: centerYAnchor))
-            dynamicConstraints.append(titleLabel.leftAnchor.constraint(equalTo: circleView.rightAnchor, constant: horizontalSpacing))
-            dynamicConstraints.append(titleLabel.centerYAnchor.constraint(equalTo: circleView.centerYAnchor))
+            constraints.append(circleView.leftAnchor.constraint(equalTo: leftAnchor))
+            constraints.append(circleView.centerYAnchor.constraint(equalTo: centerYAnchor))
+            constraints.append(titleLabel.leftAnchor.constraint(equalTo: circleView.rightAnchor, constant: horizontalSpacing))
+            constraints.append(titleLabel.centerYAnchor.constraint(equalTo: circleView.centerYAnchor))
         case .top:
-            dynamicConstraints.append(circleView.bottomAnchor.constraint(equalTo: bottomAnchor))
-            dynamicConstraints.append(circleView.centerXAnchor.constraint(equalTo: centerXAnchor))
-            dynamicConstraints.append(titleLabel.bottomAnchor.constraint(equalTo: circleView.topAnchor, constant: -verticalSpacing))
-            dynamicConstraints.append(titleLabel.centerXAnchor.constraint(equalTo: circleView.centerXAnchor))
+            constraints.append(circleView.bottomAnchor.constraint(equalTo: bottomAnchor))
+            constraints.append(circleView.centerXAnchor.constraint(equalTo: centerXAnchor))
+            constraints.append(titleLabel.bottomAnchor.constraint(equalTo: circleView.topAnchor, constant: -verticalSpacing))
+            constraints.append(titleLabel.centerXAnchor.constraint(equalTo: circleView.centerXAnchor))
         case .bottom:
-            dynamicConstraints.append(circleView.topAnchor.constraint(equalTo: topAnchor))
-            dynamicConstraints.append(circleView.centerXAnchor.constraint(equalTo: centerXAnchor))
-            dynamicConstraints.append(titleLabel.topAnchor.constraint(equalTo: circleView.bottomAnchor, constant: verticalSpacing))
-            dynamicConstraints.append(titleLabel.centerXAnchor.constraint(equalTo: circleView.centerXAnchor))
+            constraints.append(circleView.topAnchor.constraint(equalTo: topAnchor))
+            constraints.append(circleView.centerXAnchor.constraint(equalTo: centerXAnchor))
+            constraints.append(titleLabel.topAnchor.constraint(equalTo: circleView.bottomAnchor, constant: verticalSpacing))
+            constraints.append(titleLabel.centerXAnchor.constraint(equalTo: circleView.centerXAnchor))
         case .hidden:
-            dynamicConstraints.append(circleView.centerXAnchor.constraint(equalTo: centerXAnchor))
-            dynamicConstraints.append(circleView.centerYAnchor.constraint(equalTo: centerYAnchor))
+            constraints.append(circleView.centerXAnchor.constraint(equalTo: centerXAnchor))
+            constraints.append(circleView.centerYAnchor.constraint(equalTo: centerYAnchor))
         }
 
         return constraints
