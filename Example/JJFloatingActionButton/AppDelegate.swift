38b3973ab5087d45fd2ab7bf9bb8f786689f1ac5
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2023-11-07 22:41:24 +0100

    define main example color in assets

--- Example/JJFloatingActionButton/AppDelegate.swift
+++ Example/JJFloatingActionButton/AppDelegate.swift
@@ -29,8 +29,40 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window?.tintColor = UIColor(hue: 0.31, saturation: 0.37, brightness: 0.76, alpha: 1.00)
-        UINavigationBar.appearance().tintColor = UIColor.white
+        window?.tintColor = UIColor(resource: .main)
+
+        let newNavBarAppearance = customNavBarAppearance()
+
+            let appearance = UINavigationBar.appearance()
+            appearance.scrollEdgeAppearance = newNavBarAppearance
+            appearance.compactAppearance = newNavBarAppearance
+            appearance.standardAppearance = newNavBarAppearance
+            if #available(iOS 15.0, *) {
+                appearance.compactScrollEdgeAppearance = newNavBarAppearance
+            }
+
+
         return true
+    }
+
+    @available(iOS 13.0, *)
+    func customNavBarAppearance() -> UINavigationBarAppearance {
+        let customNavBarAppearance = UINavigationBarAppearance()
+        customNavBarAppearance.configureWithOpaqueBackground()
+        customNavBarAppearance.backgroundColor = UIColor(resource: .main)
+        
+        customNavBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        customNavBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+
+        let barButtonItemAppearance = UIBarButtonItemAppearance(style: .plain)
+        barButtonItemAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.white]
+        barButtonItemAppearance.disabled.titleTextAttributes = [.foregroundColor: UIColor.lightText]
+        barButtonItemAppearance.highlighted.titleTextAttributes = [.foregroundColor: UIColor.label]
+        barButtonItemAppearance.focused.titleTextAttributes = [.foregroundColor: UIColor.white]
+        customNavBarAppearance.buttonAppearance = barButtonItemAppearance
+        customNavBarAppearance.backButtonAppearance = barButtonItemAppearance
+        customNavBarAppearance.doneButtonAppearance = barButtonItemAppearance
+
+        return customNavBarAppearance
     }
 }
