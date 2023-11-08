6220abba466e31595419cc1df8246403e0499c04
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2023-11-08 13:23:53 +0100

    remove unused code

--- Sources/UIWindow+Key.swift
+++ Sources/UIWindow+Key.swift
@@ -27,10 +27,6 @@ import Foundation
 
 internal extension UIWindow {
     static var key: UIWindow? {
-        if #available(iOS 13, *) {
-            return UIApplication.shared.windows.first { $0.isKeyWindow }
-        } else {
-            return UIApplication.shared.keyWindow
-        }
+        return UIApplication.shared.windows.first { $0.isKeyWindow }
     }
 }
